dbf2915fe68085010433c5a30da09c31cf8f29a4
lovezbs <lovephp@foxmail.com>
2015-07-07 11:33:41 +0800

    改变马哥的头像,增加了readme文件

--- App/Runtime/Cache/Home/bdfd7afec0609f45a026ed666fc45c77.php
+++ App/Runtime/Cache/Home/bdfd7afec0609f45a026ed666fc45c77.php
@@ -4,18 +4,8 @@
 	<title></title>
 </head>
 <body>
-title : <?php echo ($info[0]['title']); ?>  <br/>
-contents: <?php echo ($info[0]['content']); ?> <br/>
-<hr/>
-title : <?php echo ($info[1]['title']); ?>  <br/>
-contents: <?php echo (htmlspecialchars_decode($info[1]['content'])); ?> <br/>
-<hr/>
-title : <?php echo ($info[2]['title']); ?>  <br/>
-contents: <?php echo (htmlspecialchars_decode($info[2]['content'] )); ?> <br/>
-<hr/>
 
-title : <?php echo ($info[3]['title']); ?>  <br/>
-contents: <?php echo (htmlspecialchars_decode($info[3]['content'] )); ?> <br/>
-<hr/>
+<?php if(is_array($info)): foreach($info as $key=>$v): echo ($key); ?>|<?php echo ($v["title"]); ?>:<?php echo (htmlspecialchars_decode($v["content"])); ?>
+	 <hr/><?php endforeach; endif; ?>
 </body>
 </html>
